0399975127bf7d2820205f3b6cd09d97d8b75c97
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-09-19 23:55:06 +0100

    Update TextCtrl.h

--- TextCtrl.h
+++ TextCtrl.h
@@ -1,1 +1,8 @@
+#include<wx/textctrl.h>
 
+class Potter_Ctrl:public wxTextCtrl{
+ public:
+ Potter_Ctrl(wxWindow* parent,wxWindowID id,const wxString& emptystring,const wxPoint& pos,const wxSize& sz,long style):wxTextCtrl(parent,id,emptystring,pos,sz,style){
+    
+ }
+};
