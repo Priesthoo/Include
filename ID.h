98fc59ad9e8422f709ab3d7a0f3f70b144bc8667
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-09-19 23:52:49 +0100

    Update ID.h

--- ID.h
+++ ID.h
@@ -1,1 +1,38 @@
+typedef const int cint;
 
+namespace ID{
+cint t0=0;
+cint t1=1;
+cint t2=2;
+cint t3=3;
+cint t4=4;
+cint t5=5;
+cint t6=6;
+cint t7=7;
+cint t8=8;
+cint t9=9;
+cint t10=10;
+cint t11=11;
+cint t12=12;
+cint t13=13;
+cint t14=14;
+cint t15=15;
+cint t16=16;
+cint t17=17;
+cint t18=18;
+cint t19=19;
+cint t20=20;
+cint t21=21;
+cint t22=22;
+cint t23=23;
+cint t24=24;
+cint t25=25;
+cint t26=26;
+cint t27=27;
+cint t28=28;
+cint t29=29;
+cint t30=30;
+cint t31=31;
+
+
+}
